8535161b2cae8becbdce5d45c57836a0d2132eb7
Esra Ocak <eesraunal93@gmail.com>
2023-01-05 20:18:45 +0300

    first

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addTodo } from "../features/todoSlice";
 
 const TodoInput = () => {
   const [text, setText] = useState("");
   const dispatch = useDispatch();
+  const {todo} = useSelector((state)=>state.todos)
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,7 +26,7 @@ const TodoInput = () => {
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <button type="submit" className="add-button">
+      <button type="submit" className="add-button" onClick={}>
         Add
       </button>
     </form>
